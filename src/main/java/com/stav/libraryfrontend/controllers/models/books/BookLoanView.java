1d9e2440cad698d244e4b303c7edb75c85042ba4
Lucas Åkerlund <lucas2001@hotmail.se>
2022-05-04 15:56:01 +0200

    working on a feature to loan books

--- src/main/java/com/stav/libraryfrontend/controllers/models/books/BookLoanView.java
+++ src/main/java/com/stav/libraryfrontend/controllers/models/books/BookLoanView.java
@@ -1,17 +1,34 @@
 package com.stav.libraryfrontend.controllers.models.books;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class BookLoanView extends BorderPane {
 
     private static BookLoanView instance = new BookLoanView();
 
     private BookLoanView(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/stav/libraryfrontend/fxml/books/bookLoanView.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
 
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        setup();
     }
 
     public static BookLoanView inst(){
         return instance;
+    }
+
+    private void setup(){
+
     }
 
 }
