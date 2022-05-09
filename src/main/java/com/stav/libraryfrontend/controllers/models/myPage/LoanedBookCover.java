c4002da304fa1448b9d876a5fd3818c5d83ece55
Frequill <89257706+Frequill@users.noreply.github.com>
2022-05-09 09:17:05 +0200

    small fixes, new return buttons on loaned books

--- src/main/java/com/stav/libraryfrontend/controllers/models/myPage/LoanedBookCover.java
+++ src/main/java/com/stav/libraryfrontend/controllers/models/myPage/LoanedBookCover.java
@@ -17,6 +17,9 @@ public class LoanedBookCover extends StackPane {
     @FXML
     private Label returnBookButton;
 
+    @FXML
+    private Label daysLeftLabel;
+
     public LoanedBookCover(String imageSrc){
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/stav/libraryfrontend/fxml/myPage/loanedBookCover.fxml"));
         fxmlLoader.setRoot(this);
@@ -38,6 +41,10 @@ public class LoanedBookCover extends StackPane {
             LoanedBooksView.inst().deleteBook(this);
             MyPage.inst().inputMessage("Boken har återlämnats!");
         });
+    }
+
+    public void setDaysLeft(String days){ // This should be made to work with a set date.
+        daysLeftLabel.setText(daysLeftLabel + days + " dagar");
     }
 
 }
