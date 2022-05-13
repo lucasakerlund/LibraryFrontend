a8d9d38f5975a5290d4e4710d9a8965b1cab0e3f
Lucas Åkerlund <lucas2001@hotmail.se>
2022-05-13 18:51:39 +0200

    added search bar

--- src/main/java/com/stav/libraryfrontend/controllers/models/books/Books.java
+++ src/main/java/com/stav/libraryfrontend/controllers/models/books/Books.java
@@ -2,8 +2,14 @@ package com.stav.libraryfrontend.controllers.models.books;
 
 import com.stav.libraryfrontend.abstracts.BackendCaller;
 import com.stav.libraryfrontend.models.Book;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 
@@ -16,6 +22,19 @@ public class Books extends BorderPane {
     @FXML
     private FlowPane box;
 
+    @FXML
+    private ComboBox<String> languageChoice;
+    @FXML
+    private TextField releaseInput;
+    @FXML
+    private ComboBox<String> libraryChoice;
+    @FXML
+    private TextField searchInput;
+    @FXML
+    private Label searchButton;
+    @FXML
+    private ComboBox<String> searchByChoice;
+
     private Books(){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/stav/libraryfrontend/fxml/books/books.fxml"));
         loader.setRoot(this);
@@ -26,11 +45,33 @@ public class Books extends BorderPane {
             e.printStackTrace();
         }
 
+        setup();
         updateBooks();
     }
 
     public static Books inst(){
         return instance;
+    }
+
+    private void setup(){
+        languageChoice.getItems().add("Svenska");
+        languageChoice.getItems().add("Engelska");
+
+        searchByChoice.getItems().add("Titel");
+        searchByChoice.getItems().add("Författare");
+        searchByChoice.setValue("Författare");
+
+        searchInput.setOnKeyPressed(e -> {
+            if(e.getCode() == KeyCode.ENTER){
+                searchBooks();
+            }
+        });
+
+        //Get all libraries and add them to libraryChoice box.
+    }
+
+    private void searchBooks(){
+
     }
 
     public void updateBooks(){
