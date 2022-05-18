cfe7d19b5b480940eaf098bd19e670246bf0a7d6
Frequill <89257706+Frequill@users.noreply.github.com>
2022-05-18 11:33:23 +0200

    Improved "RoomView" so that available times appear as clickable buttons that can be selected and 'booked'.
    Julius did 90% of work with 10% help from Lucas...

--- src/main/java/com/stav/libraryfrontend/controllers/models/groupRooms/RoomView.java
+++ src/main/java/com/stav/libraryfrontend/controllers/models/groupRooms/RoomView.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
@@ -20,9 +21,14 @@ public class RoomView extends BorderPane {
     private Label bookRoomButton;
 
     @FXML
-    private TextArea avalibilityTextArea; // This label tells user if room is avalible or not, use it to say when room will be avalible (date)
+    private VBox timesVbox;
 
-    public RoomView(){
+    @FXML
+    private Label messageLabel;
+
+    private TimeSelectButton focused;
+
+    public RoomView() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/stav/libraryfrontend/fxml/groupRooms/roomView.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
@@ -33,29 +39,49 @@ public class RoomView extends BorderPane {
             e.printStackTrace();
         }
 
+        messageLabel.setText("");
         setup();
     }
 
-    public void setup(){
-        placeholder();
+    public void setup() {
+        placeholder(); // Remove this when we have actual rooms from Database
 
-        bookRoomButton.setOnMousePressed(e -> {
-            //Code here to make a booking yes?
-            if (avalibilityTextArea.getText().equals("Rummet finns tillgängligt för bokning!")){
-                System.out.println("DU HAR BOKAT ETT RUM! Bra jobbat.");
-                // Set room is booked = true  <- HÄR
+        // Loop through and add all available times for booking rooms
+        addTime("05:00-07:00");
+        addTime("07:00-09:00");
+        addTime("09:00-11:00");
+        addTime("11:00-13:00");
+        addTime("13:00-15:00");
 
-            } else {
-                System.out.println("Rummet är bokat, bruh!!");
-            }
-
+        // This button must remove the available time from the room and save it for the current user
+        bookRoomButton.setOnMousePressed(e->{
+            System.out.println(focused.getTime());
+            messageLabel.setId("room-view-message-label-green");
+            messageLabel.setText("Du har bokat tiden: " + focused.getTime());
+            timesVbox.getChildren().remove(focused);
         });
+
     }
 
-    public void placeholder(){
+    private void addTime(String time){
+        TimeSelectButton selectButton = new TimeSelectButton(time);
+        selectButton.setOnMousePressed(e->{
+            if (focused != null){
+                focused.setId("");
+            }
+            focused = selectButton;
+            focused.setId("time-select-button-focused");
+        });
+
+        timesVbox.getChildren().add(selectButton);
+    }
+
+
+    public void placeholder() {
         roomName.setText("Big old room");
         descriptionArea.setText("This is a big old room, huge enough to fit me and my ego! :)");
-        avalibilityTextArea.setText("Rummet finns tillgängligt för bokning!");
     }
 
+
 }
+
